2a9c5eb2545224b4cca4fc42f879e42aa1f74724
Calvin Kirs <acm_master@163.com>
2023-06-09 22:17:07 +0800

    create Kafka docker env

--- .github/workflows/thrift.sh
+++ .github/workflows/thrift.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
-# 检查所有更改的 Thrift 文件
-for file in $(git diff --name-only HEAD^ HEAD | grep '\.thrift$'); do
-  echo "Checking changes in $file"
+          changed_files=$(git diff --name-only [origin/master] [origin/check-thrift-local] | grep '\.thrift$')
+          # 如果没有Thrift文件更改，则退出脚本
+          if [[ -z "$changed_files" ]]
+          then
+          echo "No Thrift files have been changed"
+          exit 0
+          fi
   
-  # 检查每个文件中的变更
-  while read -r line; do
-    # 判断变更类型
-    if echo "$line" | grep -q '^\+.*\bstruct\b'; then
-      # 新增结构
-      struct=$(echo "$line" | sed 's/^\+//')
-      echo "New struct: $struct"
-    elif echo "$line" | grep -q '^\+.*\bfield\b'; then
-      # 新增字段
-      field=$(echo "$line" | sed 's/^\+//')
-      echo "New field: $field"
-      if echo "$field" | grep -q '\brequired\b'; then
-        # 检查 required 字段
-        echo "Error: Required field added: $field"
-        exit 1
-      fi
-    elif echo "$line" | grep -q '^\-.*\benum\b'; then
-      # 删除枚举
-      echo "Error: Enum deleted: $line"
-      exit 1
-    fi
-  done < <(git diff HEAD^ HEAD "$file" | grep '^\+' | sed 's/^\+//')
-done
+          # 循环检查每个更改的Thrift文件
+          for file in $changed_files
+          do
+          # 提取文件名和文件路径
+          file_name=$(basename $file)
+          file_path=$(dirname $file)
+          echo "Checking Thrift file $file_name"
+          echo "Checking Thrift file $file_path"
+          
+          # 判断Thrift是新增字段还是新增结构
+          if grep -qP "struct $file_name\b" $file
+          then
+          echo "Thrift新增结构 $file_name"
+          else
+          echo "Thrift新增字段"
+          # 提取新增字段名
+          new_field=$(diff <(grep -oP 'struct \K\w+' $file_path/$file_name) <(grep -oP 'struct \K\w+' $file) | grep '>' | grep -oP '\.\K\w+')
+          # 检查新增字段是否为required
+          echo "Checking if new field $new_field is required"
+          is_required=$(grep -oP "required \K$new_field" $file)
+          if [[ "$is_required" == "required" ]]
+          then
+          echo "新增字段 $new_field 为required，出现错误"
+          exit 1
+          fi
+          fi
+          done
