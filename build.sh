da29ea9533b0e1f885a1da55c5351e4931e6df6c
Calvin Kirs <kirs@apache.org>
2023-12-28 16:38:14 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,6 +16,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
+
 ##############################################################
 # This script is used to compile Apache Doris
 # Usage:
